47c6ac34dcf1487356dcec093cc3ac138fd4fe1c
Kishan Bhatt <iamkishanbhatt@gmail.com>
2023-06-17 09:44:28 +0530

    Home page setup, authentication added, sign in with google added and mongodb connection done

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,8 @@
 import '@styles/globals.css'
 
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
+
 export const metadata = {
     title: "Promptopia",
     description: "Discover & Share AI Prompts"
@@ -9,12 +12,15 @@ const RootLayout = ({ children }) => {
     return (
         <html lang='en'>
             <body>
-                <div className='main'>
-                    <div className='gradient' />                
-                </div>
-                <main className='app'>
-                    {children}
-                </main>
+                <Provider>
+                    <div className='main'>
+                        <div className='gradient' />                
+                    </div>
+                    <main className='app'>
+                        <Nav />
+                        {children}
+                    </main>
+                </Provider>
             </body>
         </html>
     )
---
+++ components/PropmtCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PropmtCard = () => {
+  return (
+    <div>PropmtCard</div>
+  )
+}
+
+export default PropmtCard
